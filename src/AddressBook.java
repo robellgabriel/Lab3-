1897dbdf7d19286dac91433ea19fb72f525bdad0
Robell Gabriel <robellgabriel@cmail.carleton.ca>
2020-09-25 05:40:10 -0400

    I add some function calls to AddressBook.java to test Git Commit

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class AddressBook {
+
+    ArrayList<BuddyInfo> buddyInfo = new ArrayList<BuddyInfo>();
+
+    public void addBuddy(){
+
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
+
+
+

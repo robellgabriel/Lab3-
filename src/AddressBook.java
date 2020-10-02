7c813a2d9bccfabbb8b3ae9dd54b4e57b2faab02
Robell Gabriel <robellgabriel@cmail.carleton.ca>
2020-10-02 02:09:32 -0400

    changed add and remove method to match lab3 file

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,20 +2,31 @@ import java.util.*;
 
 public class AddressBook {
 
-    private ArrayList<BuddyInfo> buddyAddressBook = new ArrayList<BuddyInfo>();
+    private ArrayList<BuddyInfo> buddyAddressBook;
+
+    public AddressBook(){
+        buddyAddressBook = new ArrayList<BuddyInfo>();
+    }
 
     public void addBuddy(BuddyInfo aBuddyInfo){
-        buddyAddressBook.add(aBuddyInfo);
+        if (buddyAddressBook != null){
+            buddyAddressBook.add(aBuddyInfo);
+        }
+
     }
-    public void removeBuddy(BuddyInfo aBuddyInfo){
-        buddyAddressBook.remove(buddyAddressBook.lastIndexOf(aBuddyInfo));
+
+    public BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index < buddyAddressBook.size()){
+            return buddyAddressBook.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo();
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 
 }
